237f7d9190d0d08fe095300a249a947ef4df7809
vsahal <vsahal@utexas.edu>
2020-03-03 13:42:31 -0600

    added a couple more practice problems

---
+++ Java_A/SimpleInterest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+
+    public static void main(String[] args) {
+
+        float p, r, t, sinterest;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Principle: ");
+        p = scan.nextFloat();
+        System.out.println("Enter the Rate of Interest");
+        r = scan.nextFloat();
+        System.out.println("Enter the Time Period:");
+        t = scan.nextFloat();
+        scan.close();
+
+        sinterest = (p * r * t) / 100;
+        System.out.println("Simple Interest is: $" + sinterest);
+    }
+}
