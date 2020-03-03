23e1c69da0457882dce3fea9087419e6cb9ead37
vsahal <vsahal@utexas.edu>
2020-03-02 20:10:33 -0600

    basic adding programs

---
+++ Java_A/AddTwoNumbers.java
@@ -0,0 +1,18 @@
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        
+        int num1 = 5;
+        int num2 = 5;
+        int sum;
+
+        sum = num1 + num2;
+
+        System.out.println("Sum of these numbers: " + sum);
+
+
+
+
+
+    }
+}
